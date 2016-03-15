7a4735bf37899eb7ca0d16895504a3818dce9fa5
swinex <swinexsh@hotmail.com>
2016-03-15 14:59:07 +0800

    letter commit

--- js/data.js
+++ js/data.js
@@ -1,43 +1,61 @@
 //主要用于组织页面中所需要的数据信息
 
 $.articles = [
-  {
-	id:"never",
-	title:"NEVER",
-	type:"music",
-	attach:{
-		never_song:{
-			type:"music",
-			url:"http://data5.5sing.kgimg.com/T1tk_nBXDT1R47IVrK.mp3",
-			desc:"<< Never >> copyright belongs to SwineX."	
-		},
-		never_picture:{
-			
-			type:"pic",
-			url1:"images/never_song.jpg",
-			url2:"images/never_song.jpg",
-			desc:"Leon~"	
-		}
-	}
-  },
-  
-  {
-	id:"piece",
-	title:"平和",
-	type:"music",
-	attach:{
-		piece_song:{
-			type:"music",
-			url:"http://data8.5sing.kgimg.com/T1U5APB4ZT1R47IVrK.mp3",
-			desc:"<< 平和 >> copyright belongs to SwineX."	
-		},
-		piece_picture:{
-			
-			type:"pic",
-			url1:"images/piece.jpg",
-			url2:"images/never_song.jpg",
-			desc:"此图仅为占坑需要...~来自王国"	
-		}
-	}
-  }
+    {
+        id: "never",
+        title: "NEVER",
+        type: "music",
+        attach: {
+            never_song: {
+                type: "music",
+                url: "http://data5.5sing.kgimg.com/T1tk_nBXDT1R47IVrK.mp3",
+                desc: "<< Never >> copyright belongs to SwineX."
+            },
+            never_picture: {
+
+                type: "pic",
+                url1: "images/never_song.jpg",
+                url2: "images/never_song.jpg",
+                desc: "Leon~"
+            }
+        }
+    },
+
+    {
+        id: "piece",
+        title: "平和",
+        type: "music",
+        attach: {
+            piece_song: {
+                type: "music",
+                url: "http://data8.5sing.kgimg.com/T1U5APB4ZT1R47IVrK.mp3",
+                desc: "<< 平和 >> copyright belongs to SwineX."
+            },
+            piece_picture: {
+                type: "pic",
+                url1: "images/piece.jpg",
+                url2: "images/never_song.jpg",
+                desc: "此图仅为占坑需要...~来自王国"
+            }
+        }
+    },
+    {
+        id: "letter",
+        title: "写给你的明信片",
+        type: "article",
+        attach: {
+            letter_song: {
+                type: "music",
+                auto:1,
+                url: "http://data.5sing.kgimg.com/G033/M05/04/11/wYYBAFXh_LaAYxlBAJdnrt57Wok712.mp3",
+                desc: ""
+            },
+            letter_picture: {
+                type: "pic",
+                url1: "images/do-u.jpg",
+                desc: "twt"
+            }
+        }
+    },
+
 ];
